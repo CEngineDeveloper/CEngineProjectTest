9e38fc7098d5bfb0d53cd15fc68b1d52279b03e1
123 <123456@qq.com>
2023-04-02 20:41:33 +0800

    修改

---
+++ Packages/CEngine.CSharp/Mono/PluginConst.cs
@@ -0,0 +1,15 @@
+//------------------------------------------------------------------------------
+// PluginConst.cs
+// Copyright 2023 2023/2/28 
+// Created by CYM on 2023/2/28
+// Owner: CYM
+// 填写类的描述...
+//------------------------------------------------------------------------------
+
+namespace CYM
+{
+    public partial class SysConst
+    {
+        public const string BN_CSharp = "csharp";
+    }
+}
---
+++ Packages/CEngine.Steam/Mono/PluginGlobal.cs
@@ -0,0 +1,16 @@
+//------------------------------------------------------------------------------
+// Distribution.cs
+// Copyright 2022 2022/9/25 
+// Created by CYM on 2022/9/25
+// Owner: CYM
+// 填写类的描述...
+//------------------------------------------------------------------------------
+using CYM.Steam;
+using System;
+
+namespace CYM
+{
+    public partial class BaseGlobal : BaseCoreMono
+    {
+    }
+}
